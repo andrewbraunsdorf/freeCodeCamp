4ec11e4d558d86236fca16e10f26c451a7ac2bc8
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-05 21:27:32 +0000

    appending variables to strings

---
+++ appendingvaraiblestoStrings.js
@@ -0,0 +1,9 @@
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "impactful!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
