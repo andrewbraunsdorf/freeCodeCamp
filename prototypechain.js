c7aa0fba1dda6b05c72fb19cd087c46df5b8b1ee
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2019-08-21 17:19:22 -0400

    Create prototypechain.js

---
+++ prototypechain.js
@@ -0,0 +1,10 @@
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // => true
+
+// True
+Object.prototype.isPrototypeOf(Dog.prototype);
