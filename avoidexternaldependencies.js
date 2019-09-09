2b6ed77ba3284f8184bb67fc2c6a2590ff14f584
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2019-09-08 20:22:20 -0400

    Create avoidexternaldependencies.js

---
+++ avoidexternaldependencies.js
@@ -0,0 +1,8 @@
+var fixedValue = 4;
+
+function incrementer (fixedValue) {
+ return fixedValue + 1;
+}
+
+var newValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
